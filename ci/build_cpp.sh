19f741ced3917de67f12147ea97a1b3e9e1f2f53
Jordan Jacobelli <jjacobelli@nvidia.com>
2022-06-21 10:09:48 +0200

    Add GitHub Actions workflow for PRs
    Signed-off-by: Jordan Jacobelli <jjacobelli@nvidia.com>

---
+++ ci/build_cpp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Update env vars
+source rapids-env-update
+
+# TODO: Move in recipe build?
+export CMAKE_GENERATOR=Ninja
+
+# TODO: Move to node startup?
+export PARALLEL_LEVEL=4
+
+# TODO: Move to job config
+export CUDA=11.5
+
+# Check env
+source ci/check_env.sh
+
+################################################################################
+# BUILD - Conda package builds (LIBRMM)
+################################################################################
+
+gpuci_logger "Begin cpp build"
+
+gpuci_mamba_retry mambabuild conda/recipes/librmm
+
+rapids-upload-conda-to-s3 cpp
